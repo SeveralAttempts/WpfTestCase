20aee4ce8325d9b0b7cfbc5e685ae16469f12df5
Sergey Solomakhin <ssa@ari-on.ru>
2024-12-16 13:19:45 +0300

    xaml markup fix

--- TestHist/obj/Debug/net8.0/TestHist.AssemblyInfo.cs
+++ TestHist/obj/Debug/net8.0/TestHist.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestHist")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59328c3c78383af4f659db2cd6acab52d3ec16d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8dca09c38eda16cc998362d13b5333948addbbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestHist")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestHist")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
